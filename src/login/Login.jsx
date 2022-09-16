e6f226ef5e2284e5d9a80a5b3c63a9c5706fa1cf
andreeabrindea <andreeabrindea61@gmail.com>
2022-09-16 17:49:41 +0300

    Done with the login page :)

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,15 +1,27 @@
 import './login.css'
+import Settings from '../settings/Settings'
+import photo from '../settings/user17.png'
+import Sidebar from '../sidebar/Sidebar'
+console.log(photo)
 
 export default function Login() {
   return (
     <div className="login">
+      <div className="LoginWrapper">
         <form className="loginForm">
-            <label>Email</label>
+        <div className="settingsProfilePhoto">
+        <img className="userPhoto" src ={photo} alt="user"> 
+              </img>
+        </div>
+            <label>Email:</label>
             <input type="text" placeholder='Please enter your e-mail'/>
-            <label>Password</label>
+            <label>Password:</label>
             <input type="password" placeholder='Please enter your password'/>
-            <button className="loginButton">Login</button>
+            <button className="LoginButton">Login</button>
+            <button className="RegisterButton">Register</button>
         </form>
+      </div>
+      <Sidebar></Sidebar>
     </div>
   )
 }
--- src/settings/Settings.jsx
+++ src/settings/Settings.jsx
@@ -7,12 +7,7 @@ export default function Settings() {
   return (
     <div className="settings">
       <div className="settingsWrapper">
-      {/* <div className="settingsComponents">
-        <span className="settingsUpdateAccount"> Update </span>
-        <span className="settingsDeleteAccount"> Delete Your Account </span>
-      </div> */}
       <form className="settingsForm">
-        <p>Update Or Delete Your Account</p>
         <div className="settingsProfilePhoto">
         <img className="userPhoto" src ={photo} alt="user"> 
               </img>
@@ -25,7 +20,6 @@ export default function Settings() {
         <input type="password" placeholder='password'/>
         <button className="SubmitSettings">Update</button>
         <button className="DeleteAccount">Delete</button>
-
       </form>
       </div>
       <Sidebar></Sidebar>
